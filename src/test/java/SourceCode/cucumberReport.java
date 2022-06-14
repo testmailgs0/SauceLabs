a1c8ec7181d9423a64b0357c81a05294a2e68a53
Jarvis <testmailgs0@gmail.com>
2022-06-14 21:34:24 +0530

    Enabling Cucumber Reports

--- src/test/java/SourceCode/cucumberReport.java
+++ src/test/java/SourceCode/cucumberReport.java
@@ -2,41 +2,52 @@ package SourceCode;
 
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
+import com.intuit.karate.junit4.Karate;
 import cucumber.api.CucumberOptions;
+import io.cucumber.junit.Cucumber;
 import net.masterthought.cucumber.Configuration;
 import net.masterthought.cucumber.ReportBuilder;
 import org.apache.commons.io.FileUtils;
-import org.junit.jupiter.api.Test;
-import java.io.*;
-import java.nio.file.Paths;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.File;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.junit.Assert.assertEquals;
+
 
 /**
  *
  * @author Deepak
  */
 @CucumberOptions(features = {"classpath:SourceCode" },
-        tags = {"@TestExecutor"})
-
+        tags = {"@TestExecutor"},plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json" },
+        monochrome = true)
 public class cucumberReport {
     @Test
     public void testParallel() {
         //String karateOutputPath = "target/surefire-reports";
-        Results results = Runner.path("classpath:SourceCode").tags("@TestExecutor").parallel(0);
+        Results results = Runner.path("classpath:SourceCode").outputCucumberJson(true).tags("@TestExecutor").parallel(0);
         generateReport(results.getReportDir());
-        assertTrue(results.getFailCount() == 0, results.getErrorMessages());
+        assertEquals(results.getErrorMessages(), 0, results.getFailCount());
     }
 
     public static void generateReport(String karateOutputPath) {
-        List<File> jsonFiles = (List<File>) FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
-        List jsonPaths = new ArrayList(jsonFiles.size());
-        jsonFiles.forEach(file ->jsonPaths.add(file.getAbsoluteFile()));
-        Configuration config = new Configuration(new File("target"), "KarateBaseFramework");
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy_MM_dd_HH_mm_ss");
+        LocalDateTime now = LocalDateTime.now();
+        String currentDateTime = dtf.format(now);
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
+        ArrayList jsonPaths = new ArrayList(jsonFiles.size());
+        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+        String currentDir = System.getProperty("user.dir");
+        String folderPath= currentDir + "\\Reports\\ " + "TestCaseExecution_ " +currentDateTime;
+        File file = new File (folderPath);
+        file.mkdirs();
+        Configuration config = new Configuration( file, "SauceLabs");
         ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
         reportBuilder.generateReports();
     }
